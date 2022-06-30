e6c392b96d2147a7bf4a5708ef4b0c4513924f08
dsabelli <dsabelli3@gmail.com>
2022-06-30 09:57:11 -0400

    comment functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,24 +20,34 @@ function App() {
   const [addTask, setAddTask] = useState(false);
   const [taskTitle, setTaskTitle] = useState("");
   const [taskDescription, setTaskDescription] = useState("");
+  const [systemMessage, setSystemMessage] = useState("");
 
   const handleRegistration = async (e) => {
-    e.preventDefault();
-    await registrationService.register({
-      username,
-      email,
-      password,
-    });
-    setUsername("");
-    setEmail("");
-    setPassword("");
-    handleNewUser();
+    try {
+      e.preventDefault();
+      await registrationService.register({
+        username,
+        email,
+        password,
+      });
+      setUsername("");
+      setEmail("");
+      setPassword("");
+      handleNewUser();
+    } catch (error) {
+      setSystemMessage("System encountered an error");
+      setTimeout(() => {
+        setSystemMessage(null);
+      }, 5000);
+    }
   };
 
+  //look for a better solution to this
   const handleNewUser = () => {
     setNewUser((prevVal) => !prevVal);
   };
 
+  //function for handling user login
   const handleLogin = async (e) => {
     e.preventDefault();
     const user = await loginService.login({ username, password });
@@ -48,6 +58,7 @@ function App() {
     setPassword("");
   };
 
+  //review if this is secure, what about backend logout?
   const handleLogout = () => {
     window.localStorage.clear();
   };
@@ -59,6 +70,7 @@ function App() {
       prevTasks.map((task) => ({ ...task, isEditing: false }))
     );
   };
+
   //button to hide create task form
   const hideCreateTaskForm = (e) => {
     e.preventDefault();
@@ -96,6 +108,7 @@ function App() {
     );
   };
 
+  //button to hide task form for editing task
   const hideUpdateTaskForm = () => {
     setTaskTitle("");
     setTaskDescription("");
@@ -132,6 +145,7 @@ function App() {
     setTaskDescription("");
   };
 
+  //function to update if a task has been completed
   const handleUpdateCheck = async (id) => {
     const updatedTask = tasks.filter((task) => task.id === id)[0];
     await taskService.updateTasks({
@@ -146,6 +160,7 @@ function App() {
     );
   };
 
+  //function to handle deleting a task
   const handleDeleteTask = async (id) => {
     const deletedTask = tasks.filter((task) => task.id === id)[0];
     await taskService.deleteTasks(deletedTask);
@@ -153,6 +168,7 @@ function App() {
     setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
   };
 
+  //map out tasks into Task Components and on edit switch out for Task Form
   const taskElements = tasks.map((task) =>
     task.isEditing ? (
       <UpdateTaskForm
@@ -179,6 +195,7 @@ function App() {
     )
   );
 
+  //Get a user's tasks. Look into setting a timeout and "loading" screen
   useEffect(() => {
     const getTasks = async () => {
       const response = await taskService.getTasks(user || "");
@@ -187,6 +204,7 @@ function App() {
     getTasks();
   }, [user]);
 
+  //Checks if a user's token is stored in local storage
   useEffect(() => {
     const loggedIn = window.localStorage.getItem("loggedIn");
     if (loggedIn) {
@@ -204,6 +222,7 @@ function App() {
         newUser={newUser}
         onNewUser={handleNewUser}
       />
+      {systemMessage}
       {newUser && (
         <Register
           username={username}
