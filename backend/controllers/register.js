8460683e3fc1dac88659ef780725c7b948bf6caa
dsabelli <dsabelli3@gmail.com>
2022-07-01 17:03:38 -0400

    add password confirmation

--- backend/controllers/register.js
+++ backend/controllers/register.js
@@ -3,26 +3,19 @@ const validator = require("validator");
 const router = require("express").Router();
 const User = require("../models/user");
 
-// router.get("/", async (request, response) => {
-//   const users = await User.find({}).populate("blogs", {
-//     author: 1,
-//     title: 1,
-//     url: 1,
-//     likes: 1,
-//   });
-
-//   response.json(users);
-// });
-
 router.post("/", async (request, response) => {
-  const { username, email, password, date } = request.body;
+  const { username, email, password, confirmPassword, date } = request.body;
 
   if (!password || !validator.isStrongPassword(password)) {
     return response.status(400).json({
       error: "invalid password",
     });
   }
-
+  if (password !== confirmPassword) {
+    return response.status(400).json({
+      error: "passwords must match",
+    });
+  }
   const existingUser = await User.findOne({ username });
   if (existingUser) {
     return response.status(400).json({
