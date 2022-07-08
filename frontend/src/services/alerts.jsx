fdf60aff31dbf68108a82ffe2e7a12543e8d6fa5
dsabelli <dsabelli3@gmail.com>
2022-07-08 18:16:11 -0400

    moved sweet alterts to services

---
+++ frontend/src/services/alerts.jsx
@@ -0,0 +1,21 @@
+import Swal from "sweetalert2";
+
+const alert = (title) => {
+  const alert = Swal.fire({
+    title: "Are you sure you want to delete?",
+    text: `Project ${title}`,
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  });
+  return alert;
+};
+
+const success = (title) => {
+  console.log(title);
+  return Swal.fire("Deleted!", `Project ${title} has been deleted.`, "success");
+};
+
+export default { alert, success };
