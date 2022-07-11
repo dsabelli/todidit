92122cc25cde7e4ed33d53c8f25c14689411e5f8
dsabelli <dsabelli3@gmail.com>
2022-07-11 15:05:09 -0400

    refactor project crud out of App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { isToday, parseJSON } from "date-fns";
 import Register from "./routes/Register";
 import Login from "./routes/Login";
 import Didit from "./components/Didit";
-import ProjectForm from "./components/ProjectForm";
+import ProjectForm from "./components/Projects/ProjectForm";
 import Navbar from "./components/Navbar";
 import Menu from "./components/Menu";
 import Button from "./components/UI/Button";
@@ -16,6 +16,8 @@ import alertService from "./services/alerts";
 import CreateTask from "./components/Tasks/CreateTask";
 import ReadAndUpdateTasks from "./components/Tasks/ReadAndUpdateTasks";
 import "./App.css";
+import CreateProject from "./components/Projects/CreateProject";
+import ReadAndUpdateProjects from "./components/Projects/ReadAndUpdateProjects";
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -37,149 +39,6 @@ function App() {
   //look for a better solution to this
   const handleNewUser = () => {
     setNewUser((prevVal) => !prevVal);
-  };
-
-  //function to show create project form
-  const showCreateProjectForm = () => {
-    setProjectTitle("");
-    setAddProject((prevVal) => !prevVal);
-    setProjects((prevProjects) =>
-      prevProjects.map((project) => ({ ...project, isEditing: false }))
-    );
-  };
-
-  //function to hide create project form
-  const hideCreateProjectForm = (e) => {
-    e.preventDefault();
-    setAddProject((prevVal) => !prevVal);
-    setProjectTitle("");
-  };
-
-  //function to create projects from submission of form event
-  //posts to db with helper function and adds new task to UI
-  const handleCreateProject = async (e) => {
-    try {
-      e.preventDefault();
-      const newProject = await projectService.createProjects(
-        {
-          title: projectTitle,
-        },
-        user
-      );
-      setProjectTitle("");
-
-      setProjects((prevProjects) => prevProjects.concat(newProject));
-      showCreateProjectForm();
-    } catch (error) {
-      setSystemMessage("System encountered an error");
-      setTimeout(() => {
-        setSystemMessage(null);
-      }, 3000);
-    }
-  };
-
-  //function to show task form for editing task inline, populates fields with current data
-  //hides the current task being edited
-  const showUpdateProjectForm = (e, id) => {
-    e.stopPropagation();
-    setAddProject(false);
-    setProjectTitle(projects.filter((project) => project.id === id)[0].title);
-
-    setProjects((prevProjects) =>
-      prevProjects.map((project) =>
-        project.id === id
-          ? { ...project, isEditing: !project.isEditing }
-          : { ...project, isEditing: false }
-      )
-    );
-  };
-
-  //function to hide task form for editing project
-  //resets form fields on cancel
-  const hideUpdateProjectForm = () => {
-    setProjectTitle("");
-    setProjects((prevProjects) =>
-      prevProjects.map((project) => ({ ...project, isEditing: false }))
-    );
-  };
-
-  //function to update project from submission of form event
-  //puts to db with helper function and updates task in UI
-  const handleUpdateProject = async (e, id) => {
-    try {
-      e.preventDefault();
-      e.stopPropagation();
-      const updatedProject = projects.filter((project) => project.id === id)[0];
-      //move these into taskservice function
-
-      updatedProject.title = projectTitle;
-      updatedProject.isEditing = false;
-      await projectService.updateProjects(
-        {
-          ...updatedProject,
-        },
-        user
-      );
-
-      setProjects((prevProjects) =>
-        prevProjects.map((project) =>
-          project.id === id
-            ? {
-                ...project,
-                title: updatedProject.title,
-                isEditing: updatedProject.isEditing,
-              }
-            : project
-        )
-      );
-      setProjectTitle("");
-    } catch (error) {
-      setSystemMessage("System encountered an error");
-      setTimeout(() => {
-        setSystemMessage(null);
-      }, 3000);
-    }
-  };
-
-  //function to delete projects from submission of form event
-  //alerts user and requires confirmation on project delete
-  //deletes to db with helper function and updates projects in UI
-  //deletes all tasks with helper function and updates task in UI
-  //before deleting, a didit is created from each task object and posted with helper
-
-  const handleDeleteProject = async (e, id) => {
-    e.stopPropagation();
-    const deletedProject = projects.filter((project) => project.id === id)[0];
-    const alert = await alertService.alert(deletedProject.title);
-    if (alert.isConfirmed) {
-      try {
-        const deletedTasks = tasks.filter((task) => task.project === id);
-        for (let task of deletedTasks) {
-          const newDidit = await diditService.createDidits({ ...task }, user);
-
-          setDidits((prevDidits) => prevDidits.concat(newDidit));
-          await taskService.deleteTasks(task);
-        }
-
-        deletedProject.isArchived = true;
-        deletedProject.archivedOn = new Date();
-        await projectService.deleteProjects({ ...deletedProject }, user);
-
-        setProjects((prevProjects) =>
-          prevProjects.filter((project) => project.id !== id)
-        );
-        setTasks((prevTasks) =>
-          prevTasks.filter((task) => task.project !== id)
-        );
-
-        alertService.success(deletedProject.title);
-      } catch (error) {
-        setSystemMessage("System encountered an error");
-        setTimeout(() => {
-          setSystemMessage(null);
-        }, 3000);
-      }
-    }
   };
 
   const getDidits = async (diditTitle) => {
@@ -296,34 +155,28 @@ function App() {
       {newUser && <Register handleNewUser={handleNewUser} />}
       {!user && <Login onUser={setUser} />}
       {user && (
-        <Menu
-          tasks={tasks}
-          projects={projects}
-          title={projectTitle}
-          onTitleChange={setProjectTitle}
-          onProjectUpdate={handleUpdateProject}
-          onProjectId={setProjectId}
-          onUpdate={showUpdateProjectForm}
-          cancel={hideUpdateProjectForm}
-          onDelete={handleDeleteProject}
-        >
-          {addProject ? (
-            <ProjectForm
-              onSubmit={(e) => handleCreateProject(e)}
-              value={projectTitle}
-              onChange={(e) => setProjectTitle(e.target.value)}
-              onClick={hideCreateProjectForm}
-              submitText="add"
-            />
-          ) : (
-            user && (
-              <Button
-                onClick={() => showCreateProjectForm()}
-                className="btn mt-10 "
-                text="Add Project"
-              />
-            )
-          )}
+        <Menu>
+          <ReadAndUpdateProjects
+            user={user}
+            tasks={tasks}
+            onTasks={setTasks}
+            projects={projects}
+            onProjects={setProjects}
+            projectTitle={projectTitle}
+            onProjectTitle={setProjectTitle}
+            onAddProject={setAddProject}
+            onDidits={setDidits}
+            onSystemMessage={setSystemMessage}
+          />
+          <CreateProject
+            user={user}
+            onProjects={setProjects}
+            projectTitle={projectTitle}
+            onProjectTitle={setProjectTitle}
+            addProject={addProject}
+            onAddProject={setAddProject}
+            onSystemMessage={setSystemMessage}
+          />
         </Menu>
       )}
       {/* {//filter and display tasks due today (TEMPORARY FUNCTION!!!!)} */}
@@ -372,8 +225,6 @@ function App() {
         projectId={projectId}
         onProjectId={setProjectId}
       />
-
-      {/* {diditElements} */}
     </div>
   );
 }
