98da44931a0eee53aa5ea674c515d6c618d262b4
dsabelli <dsabelli3@gmail.com>
2022-07-12 07:51:36 -0400

    remove react router for now

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,7 @@ import App from "./App";
 import Landing from "./pages/Landing";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "./components/UI/ErrorFallback";
@@ -18,14 +19,16 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       FallbackComponent={ErrorFallback}
       onError={(error, errorInfo) => console.log({ error, errorInfo })}
     >
-      <BrowserRouter>
+      <App />
+      {/* <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={<Landing />}></Route>
-          <Route exact path="/register" element={<Register />}></Route>
-          <Route exact path="login" element={<Login />}></Route> */}
-          <Route path="/" element={<App />}></Route>
+          <Route path="/" element={<Landing />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="login" element={<Login />} />
+          <Route path="/app" element={<App />} />
+          <Route element={<ProtectedRoutes />}></Route>
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </ErrorBoundary>
   </React.StrictMode>
 );
