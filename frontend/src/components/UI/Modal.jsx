350bcd13da0db1eb96b3c2cb7fea56e3cb5c71fd
dsabelli <dsabelli3@gmail.com>
2022-07-17 09:51:45 -0400

    modify to close with button

--- frontend/src/components/UI/Modal.jsx
+++ frontend/src/components/UI/Modal.jsx
@@ -1,19 +1,31 @@
 import React from "react";
-
-const Modal = ({ title, children }) => {
+import { useNavigate } from "react-router-dom";
+const Modal = ({ title, children, checked, onChange }) => {
+  let navigate = useNavigate();
   return (
     <>
-      <label htmlFor="my-modal-4" className="btn modal-button">
+      <label htmlFor="my-modal-3" className="btn modal-button">
         {title}
       </label>
 
-      <input type="checkbox" id="my-modal-4" className="modal-toggle" />
-      <label htmlFor="my-modal-4" className="modal cursor-pointer">
-        <label className="modal-box relative" htmlFor="">
-          <h3 className="text-lg font-bold">Didit details</h3>
+      <input
+        type="checkbox"
+        id="my-modal-3"
+        className="modal-toggle"
+        defaultChecked
+      />
+      <div onChange={onChange} className="modal">
+        <div className="modal-box relative">
+          <label
+            htmlFor="my-modal-3"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
+          <h3 className="text-lg font-bold">Profile</h3>
           <div className="py-4">{children}</div>
-        </label>
-      </label>
+        </div>
+      </div>
     </>
   );
 };
