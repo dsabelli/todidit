2794cd85e81062fee7a3084751eebd03f66dfb05
dsabelli <dsabelli3@gmail.com>
2022-07-18 14:54:23 -0400

    change to config.sec

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const router = require("express").Router();
 const User = require("../models/user");
+const config = require("../utils/config");
 
 router.post("/", async (request, response) => {
   const { email, password } = request.body;
@@ -26,7 +27,7 @@ router.post("/", async (request, response) => {
     id: user._id,
   };
 
-  const token = jwt.sign(userForToken, process.env.SECRET);
+  const token = jwt.sign(userForToken, config.SECRET);
 
   response.status(200).send({ token, username: user.username, id: user._id });
 });
