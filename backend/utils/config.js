648bca271ea0d26ed58b51c095c1ada101947082
dsabelli <dsabelli3@gmail.com>
2022-07-18 14:54:15 -0400

    remove email secret

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -6,13 +6,11 @@ const MONGODB_URI =
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI;
 const SECRET = process.env.SECRET;
-const EMAIL_SECRET = process.env.EMAIL_SECRET;
 const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
 
 module.exports = {
   MONGODB_URI,
   PORT,
   SECRET,
-  EMAIL_SECRET,
   SENDGRID_API_KEY,
 };
