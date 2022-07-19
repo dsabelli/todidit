e47916f094c1c890c2b50cc8e1158efff14fc7ad
dsabelli <dsabelli3@gmail.com>
2022-07-19 13:29:23 -0400

    redo tests and remove timeout

--- backend/tests/tasks.test.js
+++ backend/tests/tasks.test.js
@@ -6,77 +6,231 @@ const app = require("../app");
 const api = supertest(app);
 const User = require("../models/user");
 const Task = require("../models/task");
+const Project = require("../models/project");
 
 describe("When a user attempts to create a task", () => {
   let token;
+  let projectId;
   beforeAll(async () => {
     await Task.deleteMany({});
     await User.deleteMany({});
+    await Project.deleteMany({});
 
     const passwordHash = await bcrypt.hash("ArnoandLily1!", 10);
     const user = new User({
-      username: "dsabelli",
       email: "dsabelli@gmail.com",
+      username: "dsabelli",
       passwordHash,
+      verified: true,
+      vToken: "abc123",
     });
-    user.save();
+
+    await user.save();
+
     const response = await api
       .post("/api/login")
-      .send({ username: "dsabelli", password: "ArnoandLily1!" });
+      .send({ email: "dsabelli@gmail.com", password: "ArnoandLily1!" })
+      .expect(200);
 
     token = response.body.token;
-  }, 100000);
 
-  it("succeeds if content valid", async () => {
-    const newTask = {
-      title: "test",
-      description: "test",
-      project: 1,
+    const project = {
+      title: "Test Project",
     };
+    const res = await api
+      .post("/api/projects")
+      .send(project)
+      .set("Authorization", `bearer ${token}`);
+
+    projectId = res.body.id;
+  });
+
+  it("succeeds if content is valid", async () => {
+    const newTask = {
+      title: "Test Task",
+      project: projectId,
+    };
+
     await api
       .post("/api/tasks")
       .send(newTask)
       .set("Authorization", `bearer ${token}`)
       .expect(201)
       .expect("Content-Type", /application\/json/);
-  }, 100000);
+  });
 
-  it("fails if missing title", async () => {
+  it("fails if title is missing", async () => {
     const newTask = {
       title: "",
-      description: "test",
-      project: 1,
+      project: projectId,
     };
+
     await api
       .post("/api/tasks")
       .send(newTask)
       .set("Authorization", `bearer ${token}`)
       .expect(400)
       .expect("Content-Type", /application\/json/);
-  }, 100000);
-}, 100000);
+  });
 
-describe("When a user attemps to modify a task", () => {
+  it("fails if project is missing", async () => {
+    const newTask = {
+      title: "Test Task",
+      project: "",
+    };
+
+    await api
+      .post("/api/tasks")
+      .send(newTask)
+      .set("Authorization", `bearer ${token}`)
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
+  });
+});
+
+describe("When a user attemps to get a task", () => {
   let token;
+  let projectId;
   let taskId;
+  let userId;
   beforeAll(async () => {
+    await Task.deleteMany({});
+    await User.deleteMany({});
+    await Project.deleteMany({});
+
+    const passwordHash = await bcrypt.hash("ArnoandLily1!", 10);
+    const user = new User({
+      email: "dsabelli@gmail.com",
+      username: "dsabelli",
+      passwordHash,
+      verified: true,
+      vToken: "abc123",
+    });
+
+    await user.save();
+
     const response = await api
       .post("/api/login")
-      .send({ username: "dsabelli", password: "ArnoandLily1!" });
+      .send({ email: "dsabelli@gmail.com", password: "ArnoandLily1!" })
+      .expect(200);
 
     token = response.body.token;
+    userId = response.body.id;
+
+    const project = {
+      title: "Test Project",
+    };
+    const res = await api
+      .post("/api/projects")
+      .send(project)
+      .set("Authorization", `bearer ${token}`);
+
+    projectId = res.body.id;
+
     const newTask = {
       title: "test",
       description: "test",
-      project: 1,
+      project: projectId,
     };
-    const res = await api
+    const r = await api
+      .post("/api/tasks")
+      .send(newTask)
+      .set("Authorization", `bearer ${token}`);
+    console.log(r.body);
+
+    taskId = r.body.id;
+  });
+
+  it("succeeds if user query found", async () => {
+    const query = {
+      params: { id: userId },
+    };
+    await api
+      .get(`/api/tasks/`)
+      .send(query)
+      .set("Authorization", `bearer ${token}`)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  it("succeeds if task id found", async () => {
+    const newTask = {
+      title: "Test Task",
+      project: projectId,
+    };
+    const response = await api
       .post("/api/tasks")
       .send(newTask)
       .set("Authorization", `bearer ${token}`);
 
-    taskId = res.body.id;
-  }, 100000);
+    taskId = response.body.id;
+
+    await api
+      .get(`/api/tasks/${taskId}`)
+      .set("Authorization", `bearer ${token}`)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  it("fails if task id not found", async () => {
+    let notReal = "92d6d4d5ce37e7f3c87558f1";
+    await api
+      .get(`/api/tasks/${notReal}`)
+      .set("Authorization", `bearer ${token}`)
+      .expect(404);
+  });
+});
+
+describe("When a user attemps to modify a task", () => {
+  let token;
+  let projectId;
+  let taskId;
+  beforeAll(async () => {
+    await Task.deleteMany({});
+    await User.deleteMany({});
+    await Project.deleteMany({});
+
+    const passwordHash = await bcrypt.hash("ArnoandLily1!", 10);
+    const user = new User({
+      email: "dsabelli@gmail.com",
+      username: "dsabelli",
+      passwordHash,
+      verified: true,
+      vToken: "abc123",
+    });
+
+    await user.save();
+
+    const response = await api
+      .post("/api/login")
+      .send({ email: "dsabelli@gmail.com", password: "ArnoandLily1!" })
+      .expect(200);
+
+    token = response.body.token;
+
+    const project = {
+      title: "Test Project",
+    };
+    const res = await api
+      .post("/api/projects")
+      .send(project)
+      .set("Authorization", `bearer ${token}`);
+
+    projectId = res.body.id;
+
+    const newTask = {
+      title: "test",
+      description: "test",
+      project: projectId,
+    };
+    const r = await api
+      .post("/api/tasks")
+      .send(newTask)
+      .set("Authorization", `bearer ${token}`);
+    console.log(r.body);
+
+    taskId = r.body.id;
+  });
 
   it("succeeds if found", async () => {
     const foundTask = await Task.findById(taskId);
@@ -89,30 +243,59 @@ describe("When a user attemps to modify a task", () => {
       .send(changedTask)
       .set("Authorization", `bearer ${token}`)
       .expect(200);
-  }, 100000);
-}, 100000);
+  });
+});
 
 describe("When a user attemps to delete a task", () => {
   let token;
   let taskId;
+  let projectId;
   beforeAll(async () => {
+    await Task.deleteMany({});
+    await User.deleteMany({});
+    await Project.deleteMany({});
+
+    const passwordHash = await bcrypt.hash("ArnoandLily1!", 10);
+    const user = new User({
+      email: "dsabelli@gmail.com",
+      username: "dsabelli",
+      passwordHash,
+      verified: true,
+      vToken: "abc123",
+    });
+
+    await user.save();
+
     const response = await api
       .post("/api/login")
-      .send({ username: "dsabelli", password: "ArnoandLily1!" });
+      .send({ email: "dsabelli@gmail.com", password: "ArnoandLily1!" })
+      .expect(200);
 
     token = response.body.token;
+
+    const project = {
+      title: "Test Project",
+    };
+    const res = await api
+      .post("/api/projects")
+      .send(project)
+      .set("Authorization", `bearer ${token}`);
+
+    projectId = res.body.id;
+
     const newTask = {
       title: "test",
       description: "test",
-      project: 1,
+      project: projectId,
     };
-    const res = await api
+    const r = await api
       .post("/api/tasks")
       .send(newTask)
       .set("Authorization", `bearer ${token}`);
+    console.log(r.body);
 
-    taskId = res.body.id;
-  }, 100000);
+    taskId = r.body.id;
+  });
 
   it("succeeds if found", async () => {
     const foundTask = await Task.findById(taskId);
@@ -121,8 +304,8 @@ describe("When a user attemps to delete a task", () => {
       .delete(`/api/tasks/${foundTask._id}`)
       .set("Authorization", `bearer ${token}`)
       .expect(204);
-  }, 100000);
-}, 100000);
+  });
+});
 afterAll(() => {
   mongoose.connection.close();
 });
