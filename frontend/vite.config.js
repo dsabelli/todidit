254e76791544d719595b5894c88c4d64f2e76172
dsabelli <dsabelli3@gmail.com>
2022-07-20 15:31:18 -0400

    install and configure vitest

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import pluginRewriteAll from "vite-plugin-rewrite-all";
@@ -5,7 +7,11 @@ import pluginRewriteAll from "vite-plugin-rewrite-all";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), pluginRewriteAll()],
-
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.js",
+  },
   server: {
     proxy: {
       "/api": {
