f9b3a055afec000fdf839a89eec44049a2cbce44
dsabelli <dsabelli3@gmail.com>
2022-07-20 08:14:10 -0400

    add loading state for pw reset

--- frontend/src/pages/Unprotected/ConfirmReset.jsx
+++ frontend/src/pages/Unprotected/ConfirmReset.jsx
@@ -7,6 +7,7 @@ import ErrorMessage from "../../components/UI/ErrorMessage";
 import Button from "../../components/UI/Button";
 import { useNavigate, useParams } from "react-router-dom";
 import UNavbar from "../../components/UNavbar";
+import { ClockLoader } from "react-spinners";
 const schema = yup.object().shape({
   password: yup
     .string()
@@ -24,6 +25,7 @@ const schema = yup.object().shape({
 const ConfirmReset = ({}) => {
   let navigate = useNavigate();
   let params = useParams();
+  const [loaded, setLoaded] = useState(true);
   const [reset, setReset] = useState(false);
   const [asyncError, setAsyncError] = useState("");
   const {
@@ -39,6 +41,7 @@ const ConfirmReset = ({}) => {
     handleConfirmReset({ password, confirmPassword });
 
   const handleConfirmReset = async ({ password, confirmPassword }) => {
+    setLoaded(false);
     try {
       await userService.confirmReset(
         {
@@ -47,9 +50,11 @@ const ConfirmReset = ({}) => {
         },
         params.token
       );
+      setLoaded(true);
       setReset(true);
       setAsyncError("");
     } catch (error) {
+      setLoaded(true);
       console.log(error);
       const errorMsg = error.response ? error.response.data.error : error;
       setAsyncError(errorMsg);
@@ -66,61 +71,65 @@ const ConfirmReset = ({}) => {
       }, 3000);
   }, [reset]);
 
-  return reset ? (
-    <>
-      <UNavbar />
-      <div>Password reset confirmed! Taking you to the login page...</div>
-    </>
-  ) : (
-    <>
-      <UNavbar />
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Confirm New Password</h1>
-          </div>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-              <div className="card-body">
-                <div className="form-control">
-                  <input
-                    type="password"
-                    name="password"
-                    {...register("password")}
-                    placeholder="new password"
-                    className="input input-bordered"
-                  />
-                  {errors.password && (
-                    <ErrorMessage errorMessage={errors.password?.message} />
-                  )}
-                </div>
-                <div className="form-control">
-                  <input
-                    type="password"
-                    name="confirmPassword"
-                    {...register("confirmPassword")}
-                    placeholder="new confirm password"
-                    className="input input-bordered"
-                  />
-                  {errors.confirmPassword && (
-                    <ErrorMessage
-                      errorMessage={errors.confirmPassword?.message}
+  return loaded ? (
+    reset ? (
+      <>
+        <UNavbar />
+        <div>Password reset confirmed! Taking you to the login page...</div>
+      </>
+    ) : (
+      <>
+        <UNavbar />
+        <div className="hero min-h-screen bg-base-200">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="text-center lg:text-left">
+              <h1 className="text-5xl font-bold">Confirm New Password</h1>
+            </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card-body">
+                  <div className="form-control">
+                    <input
+                      type="password"
+                      name="password"
+                      {...register("password")}
+                      placeholder="new password"
+                      className="input input-bordered"
                     />
-                  )}
-                </div>
-                <div className="form-control mt-6">
-                  <Button
-                    text={"Confirm"}
-                    type="submit"
-                    className={"btn-primary"}
-                  />
+                    {errors.password && (
+                      <ErrorMessage errorMessage={errors.password?.message} />
+                    )}
+                  </div>
+                  <div className="form-control">
+                    <input
+                      type="password"
+                      name="confirmPassword"
+                      {...register("confirmPassword")}
+                      placeholder="new confirm password"
+                      className="input input-bordered"
+                    />
+                    {errors.confirmPassword && (
+                      <ErrorMessage
+                        errorMessage={errors.confirmPassword?.message}
+                      />
+                    )}
+                  </div>
+                  <div className="form-control mt-6">
+                    <Button
+                      text={"Confirm"}
+                      type="submit"
+                      className={"btn-primary"}
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
-      </div>
-    </>
+      </>
+    )
+  ) : (
+    <ClockLoader />
   );
 };
 
--- frontend/src/pages/Unprotected/ResetPassword.jsx
+++ frontend/src/pages/Unprotected/ResetPassword.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
 import userService from "../../services/users";
 import { useForm } from "react-hook-form";
-import { Link, useNavigate } from "react-router-dom";
+// import { Link, useNavigate } from "react-router-dom";
 import Button from "../../components/UI/Button";
 import ErrorMessage from "../../components/UI/ErrorMessage";
+import { ClockLoader } from "react-spinners";
 
 const ResetPassword = () => {
-  let navigate = useNavigate();
+  // let navigate = useNavigate();
+  const [loaded, setLoaded] = useState(true);
   const [reset, setReset] = useState(false);
   const [asyncError, setAsyncError] = useState("");
   const {
@@ -18,10 +20,13 @@ const ResetPassword = () => {
 
   //function for handling user login
   const handleReset = async ({ email }) => {
+    setLoaded(false);
     try {
       await userService.resetPassword({ email });
+      setLoaded(true);
       setReset(true);
     } catch (error) {
+      setLoaded(true);
       console.log(error);
       let errorMsg = error.response.data.error || error;
       setAsyncError(errorMsg);
@@ -31,53 +36,57 @@ const ResetPassword = () => {
     }
   };
 
-  useEffect(() => {
-    reset &&
-      setTimeout(() => {
-        navigate("/");
-      }, 5000);
-  }, [reset]);
+  // useEffect(() => {
+  //   reset &&
+  //     setTimeout(() => {
+  //       navigate("/");
+  //     }, 5000);
+  // }, [reset]);
 
-  return reset ? (
-    <div>pw reset, check email</div>
-  ) : (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Reset Password</h1>
-          <p className="py-6">
-            Enter your user account's verified email address and we will send
-            you a password reset link.
-          </p>
-        </div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <div className="card-body">
-              <div className="form-control">
-                {asyncError && <ErrorMessage errorMessage={asyncError} />}
-                <input
-                  autoFocus
-                  required
-                  type="text"
-                  name="email"
-                  placeholder="enter your email"
-                  className="input input-bordered"
-                  {...register("email")}
-                />
-              </div>
+  return loaded ? (
+    reset ? (
+      <div>pw reset, check email</div>
+    ) : (
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Reset Password</h1>
+            <p className="py-6">
+              Enter your user account's verified email address and we will send
+              you a password reset link.
+            </p>
+          </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+              <div className="card-body">
+                <div className="form-control">
+                  {asyncError && <ErrorMessage errorMessage={asyncError} />}
+                  <input
+                    autoFocus
+                    required
+                    type="text"
+                    name="email"
+                    placeholder="enter your email"
+                    className="input input-bordered"
+                    {...register("email")}
+                  />
+                </div>
 
-              <div className="form-control mt-6">
-                <Button
-                  type="submit"
-                  text={"Reset"}
-                  className={"btn-primary"}
-                />
+                <div className="form-control mt-6">
+                  <Button
+                    type="submit"
+                    text={"Reset"}
+                    className={"btn-primary"}
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
-    </div>
+    )
+  ) : (
+    <ClockLoader />
   );
 };
 
