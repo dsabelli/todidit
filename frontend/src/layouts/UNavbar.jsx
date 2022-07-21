55217db3ae5e8f395bcc2bbdaec8c0ab77763ca1
dsabelli <dsabelli3@gmail.com>
2022-07-21 15:04:36 -0400

    styling!

--- frontend/src/layouts/UNavbar.jsx
+++ frontend/src/layouts/UNavbar.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Button from "../components/UI/Button";
+import ToDidit from "../Assets/toDidit";
 
 const UNavbar = ({ isLanding }) => {
   return (
-    <nav className="navbar bg-base-100 flex px-4">
-      <div className="flex-1">
-        <Link to="/">
-          <p className="btn btn-ghost normal-case text-xl">toDidit</p>
+    <nav className="navbar bg-base-100 flex px-4 justify-between">
+      <div className="flex-1 btn btn-ghost gap-4 justify-start max-w-fit">
+        <Link className="flex gap-4" to="/">
+          <ToDidit className={"w-8"} themeLines="#FFF" themeCheck="green" />
+          <p className=" normal-case text-xl">toDidit</p>
         </Link>
       </div>
-      <div className={`flex gap-4 ${isLanding ? "" : "hidden"}`}>
+      <div className={`flex gap-4 ${isLanding ? "" : "hidden"} `}>
         <Link to="/login">
           {" "}
-          <Button text={"Login"} />
+          <Button text={"Login"} className="btn-base-content" />
         </Link>
       </div>
     </nav>
--- frontend/src/pages/Unprotected/Landing.jsx
+++ frontend/src/pages/Unprotected/Landing.jsx
@@ -1,16 +1,26 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import UNavbar from "../../layouts/UNavbar";
+import Button from "../../components/UI/Button";
 import Hero from "../../components/UI/Hero";
+import HeroSvg from "../../Assets/HeroSvg";
 const Landing = () => {
-  let navigate = useNavigate();
   return (
     <div>
       <UNavbar isLanding />
       <Hero
-        onClick={() => {
-          navigate("/register");
-        }}
-      />
+        text={
+          <>
+            <h1 className="text-6xl font-bold">toDidit</h1>
+            <p className="pt-6 ">Keep track of what you need to do,</p>
+            <p className="pb-6 ">and what you've already done.</p>
+            <Link to="/register">
+              <Button className="btn btn-primary" text={"Get Started"} />
+            </Link>
+          </>
+        }
+      >
+        <HeroSvg className={"flex w-80"} />
+      </Hero>
     </div>
   );
 };
