976882d6d3d70b80e5fc05f9c9ec16ffd70c4585
dsabelli <dsabelli3@gmail.com>
2022-07-21 15:04:25 -0400

    refactor to make reusable

--- frontend/src/components/UI/Hero.jsx
+++ frontend/src/components/UI/Hero.jsx
@@ -1,22 +1,9 @@
-import Button from "./Button";
-
-const Hero = ({ onClick }) => {
+const Hero = ({ children, text }) => {
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <Button
-            className="btn btn-primary"
-            onClick={onClick}
-            text={"Get Started"}
-          />
-        </div>
+    <div className="hero md:min-h-screen bg-base-200">
+      <div className="hero-content flex-col-reverse md:flex-row-reverse flex-1">
+        {children}
+        <div className="flex-1">{text}</div>
       </div>
     </div>
   );
