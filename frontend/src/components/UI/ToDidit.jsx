4703b8252ca3e3d41544e26129a48a8825b24d69
dsabelli <dsabelli3@gmail.com>
2022-07-22 17:20:18 -0400

    classname props

--- frontend/src/components/UI/ToDidit.jsx
+++ frontend/src/components/UI/ToDidit.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../Assets/Logo";
 
-const ToDidit = ({ user }) => {
+const ToDidit = ({ user, className }) => {
   return (
-    <div className=" btn btn-ghost gap-4  max-w-fit">
+    <div className={`btn btn-ghost gap-4  max-w-fit ${className}`}>
       <Link className="flex gap-4" to={user ? "/app/today" : "/"}>
         <Logo className={"w-8"} themeLines="#FFF" themeCheck="green" />
         <p className=" normal-case text-xl">toDidit</p>
