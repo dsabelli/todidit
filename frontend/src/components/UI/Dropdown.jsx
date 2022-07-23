c0b47291df29b6632b6a865df8a948d2529befae
dsabelli <dsabelli3@gmail.com>
2022-07-23 12:57:43 -0400

    add props

--- frontend/src/components/UI/Dropdown.jsx
+++ frontend/src/components/UI/Dropdown.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-const Dropdown = ({ children, text }) => {
+const Dropdown = ({ text, onClick, className, children }) => {
   return (
-    <div className="dropdown">
+    <div className={`dropdown ${className}`} onClick={onClick}>
       <label tabIndex="0" className="btn m-1">
-        {text}
+        {text || children}
       </label>
       <ul
+        onClick={(e) => e.target.blur()}
         tabIndex="0"
         className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
       >
