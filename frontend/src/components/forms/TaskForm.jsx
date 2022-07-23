fea46e5d19eabdfa8fe68fc46912cd52ccc3f0f8
dsabelli <dsabelli3@gmail.com>
2022-07-23 12:57:22 -0400

    change datepicker to dropdown

--- frontend/src/components/forms/TaskForm.jsx
+++ frontend/src/components/forms/TaskForm.jsx
@@ -30,59 +30,60 @@ const TaskForm = ({
   );
 
   return (
-    <form onSubmit={onSubmit}>
-      <div>
-        <label htmlFor="Title"></label>
-        <Input
-          autoFocus
-          placeholder="e.g., style this project better"
-          id="Title"
-          type="text"
-          value={titleValue}
-          name="Title"
-          onChange={onTitleChange}
-          className="input input-ghost w-full max-w-xs"
-        />
-      </div>
-      <div>
-        <label htmlFor="Description"></label>
-        <Textarea
-          id="Description"
-          placeholder="Description"
-          type="text"
-          value={descriptionValue}
-          name="Description"
-          onChange={onDescriptionChange}
-          className="textarea textarea-ghost resize-none w-full max-w-xs h-24"
-        />
-      </div>
-      <div>
-        <DueDate dueDate={dueDate} onDueDate={onDueDate} />
-      </div>
-      <div>
-        <div className="dropdown dropdown-hover">
-          <label tabIndex="0" className="btn m-1">
-            {selectedProject || projectTitle || "Select"}
-          </label>
-          <ul
-            tabIndex="0"
-            className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            {projectElements}
-          </ul>
+    <div className="pl-6">
+      <form onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="Title"></label>
+          <Input
+            autoFocus
+            placeholder="e.g., style this project better"
+            id="Title"
+            type="text"
+            value={titleValue}
+            name="Title"
+            onChange={onTitleChange}
+            className="input input-ghost p-0 w-full text-xl "
+          />
         </div>
-      </div>
-      <Button
-        className={
-          (titleValue && projectTitle) || (titleValue && projectId)
-            ? "btn"
-            : "btn btn-disabled opacity-50"
-        }
-        type="submit"
-        text={submitText}
-      />
-      <Button text="cancel" onClick={onClick} />
-    </form>
+        <div className="">
+          <label htmlFor="Description"></label>
+          <Textarea
+            id="Description"
+            placeholder="Description"
+            type="text"
+            value={descriptionValue}
+            name="Description"
+            onChange={onDescriptionChange}
+            className="textarea textarea-ghost resize-none w-full p-0 h-20 max-h-20"
+          />
+        </div>
+        <div className="flex justify-between mt-2">
+          <DueDate dueDate={dueDate} onDueDate={onDueDate} />
+
+          <div className="dropdown dropdown-hover">
+            <label tabIndex="0" className="btn m-1">
+              {selectedProject || projectTitle || "Select"}
+            </label>
+            <ul
+              tabIndex="0"
+              className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {projectElements}
+            </ul>
+          </div>
+        </div>
+        <Button
+          className={
+            (titleValue && projectTitle) || (titleValue && projectId)
+              ? "btn"
+              : "btn btn-disabled opacity-50"
+          }
+          type="submit"
+          text={submitText}
+        />
+        <Button text="cancel" onClick={onClick} />
+      </form>
+    </div>
   );
 };
 
