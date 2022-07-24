7f10aa6ddeb095296f53f8de3a86345866670f49
dsabelli <dsabelli3@gmail.com>
2022-07-24 16:01:21 -0400

    new icon assets

---
+++ frontend/src/Assets/ChevronIcons.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const ChevronIconRight = ({ classNameRight }) => {
+  return (
+    <div className={` ${classNameRight}`}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="feather feather-chevron-right"
+      >
+        <polyline points="9 18 15 12 9 6"></polyline>
+      </svg>
+    </div>
+  );
+};
+
+const ChevronIconDown = ({ classNameDown }) => {
+  return (
+    <div className={` ${classNameDown}`}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="feather feather-chevron-down"
+      >
+        <polyline points="6 9 12 15 18 9"></polyline>
+      </svg>
+    </div>
+  );
+};
+
+export { ChevronIconRight, ChevronIconDown };
