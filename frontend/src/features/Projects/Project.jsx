4df5e8fd49be9fa15e5107454aa23a6d3e80d6bd
dsabelli <dsabelli3@gmail.com>
2022-07-24 16:04:29 -0400

    project submenu, stopProp for btns

--- frontend/src/features/Projects/Project.jsx
+++ frontend/src/features/Projects/Project.jsx
@@ -1,30 +1,43 @@
 import Button from "../../components/UI/Button";
+import EditIcon from "../../Assets/EditIcon";
 import DeleteIcon from "../../Assets/DeleteIcon";
 import { Link } from "react-router-dom";
 
 const Project = ({ id, onUpdate, onDelete, title }) => {
   return (
-    <div className="flex justify-between  ">
-      <Link to={`project/${id}`}>
-        <div className="">{title}</div>
-      </Link>
-
-      <div className="flex gap-4">
-        <Button
-          onClick={() => {
-            onUpdate(id);
-          }}
-          text="edit"
-        />
-        <Button
-          className={`btn btn-square `}
-          onClick={() => {
-            onDelete();
-          }}
-          text={<DeleteIcon />}
-        />
-      </div>
-    </div>
+    <ul className="menu text-base ">
+      <li className="">
+        <Link to={`project/${id}`}>
+          <div className="p-0 flex justify-between w-full">
+            <div className="">{title}</div>
+            <div className="flex ">
+              <Button
+                className={
+                  "btn-xs w-6 h-6 p-1 bg-transparent border-none hover:bg-base-300"
+                }
+                onClick={(e) => {
+                  e.stopPropagation();
+                  e.preventDefault();
+                  onUpdate(id);
+                }}
+              >
+                <EditIcon className={"w-6 h-6 hover:text-primary"} />
+              </Button>
+              <Button
+                className={`btn-xs w-6 h-6 p-1 bg-transparent border-none hover:bg-base-300`}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  e.preventDefault();
+                  onDelete();
+                }}
+              >
+                <DeleteIcon className={"w-6 hover:text-error"} />
+              </Button>
+            </div>
+          </div>
+        </Link>
+      </li>
+    </ul>
   );
 };
 
