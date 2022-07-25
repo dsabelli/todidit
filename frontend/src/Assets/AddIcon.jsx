7ae40af5d1872aebc4068adf0e1d32b0ca7d8aa5
dsabelli <dsabelli3@gmail.com>
2022-07-25 09:01:52 -0400

    new asset add icon

---
+++ frontend/src/Assets/AddIcon.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const AddIcon = ({ className }) => {
+  return (
+    <div className={` ${className}`}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="feather feather-plus"
+      >
+        <line x1="12" y1="5" x2="12" y2="19"></line>
+        <line x1="5" y1="12" x2="19" y2="12"></line>
+      </svg>
+    </div>
+  );
+};
+
+export default AddIcon;
