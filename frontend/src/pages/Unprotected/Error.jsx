a63010617aee2cbbdf19e8c020f958fd325ec5e6
dsabelli <dsabelli3@gmail.com>
2022-07-25 14:26:29 -0400

    add hero to error page

--- frontend/src/pages/Unprotected/Error.jsx
+++ frontend/src/pages/Unprotected/Error.jsx
@@ -1,12 +1,29 @@
 import React from "react";
-import UNavbar from "../../layouts/UNavbar";
-
-const Error = () => {
+import Hero from "../../components/UI/Hero";
+import ErrorSvg from "../../Assets/ErrorSvg";
+const Error = ({ error }) => {
   return (
-    <>
-      <UNavbar />
-      <div>Error</div>
-    </>
+    <div role="alert">
+      <Hero
+        className={"w-screen"}
+        text={
+          <div className=" px-20 md:ml-20 md:p-0">
+            <h1 className="text-6xl font-bold">{error.message || "Oh NO!"}</h1>
+            <p className="pt-6 ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+              ipsum nisi provident maxime rem illum voluptas beatae, blanditiis
+              dolorem?
+            </p>
+            <p className="pb-6 ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias,
+              iusto. Ducimus velit laboriosam molestiae?
+            </p>
+          </div>
+        }
+      >
+        <ErrorSvg className={"text-error w-1/3 md:w-1/2"} />
+      </Hero>
+    </div>
   );
 };
 
