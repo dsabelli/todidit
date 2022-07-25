35abe93c2d5f29a6a62da43377523e9c9202d684
dsabelli <dsabelli3@gmail.com>
2022-07-25 17:27:09 -0400

    change bg-color

--- frontend/src/components/UI/Hero.jsx
+++ frontend/src/components/UI/Hero.jsx
@@ -1,11 +1,11 @@
 const Hero = ({ children, text, className }) => {
   return (
-    <div className="hero md:min-h-screen bg-base-200 ">
+    <div className="hero md:min-h-screen bg-base-100">
       <div
-        className={`hero-content flex-col-reverse md:flex-row-reverse items-center flex-1 ${className}`}
+        className={`hero-content flex-col-reverse md:flex-row-reverse items-center flex-1 w-full ${className}`}
       >
         {children}
-        <div className="flex-1">{text}</div>
+        <div className="">{text}</div>
       </div>
     </div>
   );
