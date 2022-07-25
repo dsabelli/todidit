d1aede28724b22a3bd7556b29ebe39c0bb906dea
dsabelli <dsabelli3@gmail.com>
2022-07-25 14:24:57 -0400

    add svg assets

---
+++ frontend/src/Assets/ErrorIcon.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const ErrorIcon = ({ className }) => {
+  return (
+    <div className={` ${className}`}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="feather feather-alert-circle"
+      >
+        <circle cx="12" cy="12" r="10"></circle>
+        <line x1="12" y1="8" x2="12" y2="12"></line>
+        <line x1="12" y1="16" x2="12.01" y2="16"></line>
+      </svg>
+    </div>
+  );
+};
+
+export default ErrorIcon;
