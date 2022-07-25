99b3adc91aa02d31911b411ce1eab864afd19f68
dsabelli <dsabelli3@gmail.com>
2022-07-25 14:25:24 -0400

    check localstorage

--- frontend/src/components/UI/ToDidit.jsx
+++ frontend/src/components/UI/ToDidit.jsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 import Logo from "../../Assets/Logo";
 
 const ToDidit = ({ user, className }) => {
+  let loggedIn = window.localStorage.getItem("loggedIn");
+
   return (
     <div className={`btn btn-ghost gap-4  max-w-fit ${className}`}>
-      <Link className="flex gap-4" to={user ? "/app/today" : "/"}>
+      <Link className="flex gap-4" to={loggedIn ? "/app/today" : "/"}>
         <Logo className={"w-8"} themeLines="#FFF" themeCheck="green" />
         <p className=" normal-case text-xl">toDidit</p>
       </Link>
