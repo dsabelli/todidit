a35d5bd1a2c66a1cdbe390b09b19a9bee77016d1
dsabelli <dsabelli3@gmail.com>
2022-07-27 13:05:37 -0400

    make font smaller

--- frontend/src/pages/Unprotected/Error.jsx
+++ frontend/src/pages/Unprotected/Error.jsx
@@ -8,7 +8,7 @@ const Error = ({ error }) => {
         className={"w-screen"}
         text={
           <div className=" px-20 md:ml-20 md:p-0">
-            <h1 className="text-6xl font-bold">{error.message || "Uh oh!"}</h1>
+            <h1 className="text-3xl font-bold">{error.message || "Uh oh!"}</h1>
             {!error && <p className="pt-6 ">Something went wrong.</p>}
             {!error && (
               <p className="pb-6 ">Please go back or try again later.</p>
