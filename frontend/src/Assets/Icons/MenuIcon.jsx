3b458a3ccfdd090a0c254b84388e44d622385b66
dsabelli <dsabelli3@gmail.com>
2022-07-27 12:29:15 -0400

    simplify

--- frontend/src/Assets/Icons/MenuIcon.jsx
+++ frontend/src/Assets/Icons/MenuIcon.jsx
@@ -1,31 +1,21 @@
 import React from "react";
 
-const MenuIcon = ({ className, onClick }) => {
+const MenuIcon = ({ className }) => {
   return (
-    <div onClick={onClick} className={` ${className}`}>
-      <label className="swap swap-rotate">
-        <input type="checkbox" />
-
-        <svg
-          className="swap-off fill-current"
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          viewBox="0 0 512 512"
-        >
-          <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
-        </svg>
-
-        <svg
-          className="swap-on fill-current"
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          viewBox="0 0 512 512"
-        >
-          <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
-        </svg>
-      </label>
+    <div className={`text-neutral ${className}`}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        className="inline-block stroke-current"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M4 6h16M4 12h16M4 18h16"
+        ></path>
+      </svg>
     </div>
   );
 };
