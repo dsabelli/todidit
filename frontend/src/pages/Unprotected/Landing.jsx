71c4b8f6a1b9deae647f811c9b065cdac650ebb4
dsabelli <dsabelli3@gmail.com>
2022-07-28 09:48:53 -0400

    change responsiveness

--- frontend/src/pages/Unprotected/Landing.jsx
+++ frontend/src/pages/Unprotected/Landing.jsx
@@ -12,14 +12,16 @@ const Landing = () => {
           text={
             <div className="flex flex-col items-center text-left">
               <div className="w/1-2">
-                <h1 className="text-8xl font-bold">toDidit</h1>
-                <p className="text-2xl pt-6 ">
+                <h1 className="text-6xl md:text-8xl font-bold">toDidit</h1>
+                <p className="text-lg md:text-2xl pt-6 ">
                   Keep track of what you need to do,
                 </p>
-                <p className="text-2xl pb-6 ">and what you've already done.</p>
+                <p className="text-lg md:text-2xl pb-6 ">
+                  and what you've already done.
+                </p>
                 <Link to="/register">
                   <Button
-                    className="btn-lg btn-primary  hover:bg-secondary"
+                    className=" md:btn-lg btn-primary  hover:bg-secondary"
                     text={"Get Started"}
                   />
                 </Link>
@@ -27,7 +29,7 @@ const Landing = () => {
             </div>
           }
         >
-          <HeroSvg className={"w-1/2"} />
+          <HeroSvg className={" w-80 mt-12 md:mt-0 md:w-1/2"} />
         </Hero>
       </div>
     </>
