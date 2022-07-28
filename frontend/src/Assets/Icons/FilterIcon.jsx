48103909a6110df6b5b3a55de892040dd64047a6
dsabelli <dsabelli3@gmail.com>
2022-07-28 15:13:04 -0400

    new icons for task organization

---
+++ frontend/src/Assets/Icons/FilterIcon.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const FilterIcon = ({ className }) => {
+  return (
+    <div className={`text-base-content ${className}`}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="feather feather-filter"
+      >
+        <polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"></polygon>
+      </svg>
+    </div>
+  );
+};
+
+export default FilterIcon;
---
+++ frontend/src/Assets/Icons/GridIcon.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const GridIcon = ({ className }) => {
+  return (
+    <div className={`text-base-content ${className}`}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="feather feather-grid"
+      >
+        <rect x="3" y="3" width="7" height="7"></rect>
+        <rect x="14" y="3" width="7" height="7"></rect>
+        <rect x="14" y="14" width="7" height="7"></rect>
+        <rect x="3" y="14" width="7" height="7"></rect>
+      </svg>
+    </div>
+  );
+};
+
+export default GridIcon;
