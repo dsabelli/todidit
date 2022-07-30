7ed0bd14cd4d0afd5a607af47bafe4973eb778f4
dsabelli <dsabelli3@gmail.com>
2022-07-30 08:25:20 -0400

    add delete all button

--- frontend/src/pages/Protected/Completed.jsx
+++ frontend/src/pages/Protected/Completed.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useContext } from "react";
 import { useOutletContext } from "react-router-dom";
+import Button from "../../components/UI/Button";
 import { UserContext } from "../../context/UserContext";
 import ReadAndUpdateTasks from "../../features/Tasks/ReadAndUpdateTasks";
-
+import taskService from "../../services/tasks";
+import diditService from "../../services/didits";
+import alertService from "../../services/alerts";
 const Completed = () => {
   const { user } = useContext(UserContext);
   const [
@@ -25,6 +28,34 @@ const Completed = () => {
     setTasks((prevTasks) => prevTasks.filter((task) => task.isChecked));
   }, [allTasks]);
 
+  const handleDeleteCompletedTasks = async () => {
+    const alert = await alertService.alert("Completed Tasks");
+
+    if (alert.isConfirmed) {
+      try {
+        const deletedTasks = tasks.filter((task) => task.isChecked);
+        for (let task of deletedTasks) {
+          task.completedOn ? null : (task.completedOn = new Date());
+          await diditService.createDidits({ ...task }, user);
+          await taskService.deleteTasks(task);
+
+          setTasks((prevTasks) =>
+            prevTasks.filter((prevTask) => prevTask.id !== task.id)
+          );
+          setAllTasks((prevTasks) =>
+            prevTasks.filter((prevTask) => prevTask.id !== task.id)
+          );
+        }
+        alertService.success("Idiot!");
+      } catch (error) {
+        console.log(error);
+        setSystemMessage("System encountered an error");
+        setTimeout(() => {
+          setSystemMessage(null);
+        }, 3000);
+      }
+    }
+  };
   return (
     <div>
       <ReadAndUpdateTasks
@@ -40,6 +71,7 @@ const Completed = () => {
         projectId={projectId}
         onProjectId={setProjectId}
       />
+      <Button onClick={() => handleDeleteCompletedTasks()}>Delete All</Button>
     </div>
   );
 };
