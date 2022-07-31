26ff2fdc222e2032676f88a495d76e0d06c47100
dsabelli <dsabelli3@gmail.com>
2022-07-31 12:30:01 -0400

    if user, navbar with avatar, else no

--- frontend/src/pages/Unprotected/Contact.jsx
+++ frontend/src/pages/Unprotected/Contact.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import { useEffect, useContext } from "react";
 import ContactSvg from "../../Assets/SVGs/ContactSvg";
 import UNavbar from "../../layouts/UNavbar";
+import { UserContext } from "../../context/UserContext";
+import taskService from "../../services/tasks";
 
 const Contact = () => {
+  const { user, setUser } = useContext(UserContext);
+
+  useEffect(() => {
+    const loggedIn = window.localStorage.getItem("loggedIn");
+    if (loggedIn) {
+      const user = JSON.parse(loggedIn);
+      taskService.setToken(user.token);
+      setUser(user);
+      // setLoaded(true);
+      // setTimeout(() => {
+      //   setLoaded(true);
+      // }, 400);
+    }
+  }, []);
   return (
     <div>
-      <UNavbar />
+      {user ? <UNavbar username={user.username} /> : <UNavbar />}
       Contact <ContactSvg className="" />
     </div>
   );
