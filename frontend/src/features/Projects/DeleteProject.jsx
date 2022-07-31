a3e3f75b0a55c8ab8d3a8c43d72cb5e9944da568
dsabelli <dsabelli3@gmail.com>
2022-07-31 10:41:38 -0400

    remove sweetalerts

--- frontend/src/features/Projects/DeleteProject.jsx
+++ frontend/src/features/Projects/DeleteProject.jsx
@@ -1,7 +1,6 @@
 import projectService from "../../services/projects";
 import taskService from "../../services/tasks";
 import diditService from "../../services/didits";
-import alertService from "../../services/alerts";
 
 //function to delete projects from submission of form event
 //alerts user and requires confirmation on project delete
@@ -15,37 +14,33 @@ const handleDeleteProject = async (
   navigate
 ) => {
   const deletedProject = projects.find((project) => project.id === id);
-  const alert = await alertService.alert(deletedProject.title);
-  if (alert.isConfirmed) {
-    try {
-      const deletedTasks = tasks.filter((task) => task.project === id);
-      for (let task of deletedTasks) {
-        task.completedOn ? null : (task.completedOn = new Date());
-        await diditService.createDidits({ ...task }, user);
-        await taskService.deleteTasks(task);
-      }
 
-      deletedProject.isArchived = true;
-      deletedProject.archivedOn = new Date();
-      await projectService.deleteProjects({ ...deletedProject }, user);
-
-      onProjects((prevProjects) =>
-        prevProjects.map((project) =>
-          project.id === id ? { ...deletedProject } : project
-        )
-      );
-      onTasks((prevTasks) => prevTasks.filter((task) => task.project !== id));
-      onAllTasks((prevTasks) =>
-        prevTasks.filter((task) => task.project !== id)
-      );
-      alertService.success(deletedProject.title);
-      navigate("/app/today");
-    } catch (error) {
-      onSystemMessage("System encountered an error");
-      setTimeout(() => {
-        onSystemMessage(null);
-      }, 3000);
+  try {
+    const deletedTasks = tasks.filter((task) => task.project === id);
+    for (let task of deletedTasks) {
+      task.completedOn ? null : (task.completedOn = new Date());
+      await diditService.createDidits({ ...task }, user);
+      await taskService.deleteTasks(task);
     }
+
+    deletedProject.isArchived = true;
+    deletedProject.archivedOn = new Date();
+    await projectService.deleteProjects({ ...deletedProject }, user);
+
+    onProjects((prevProjects) =>
+      prevProjects.map((project) =>
+        project.id === id ? { ...deletedProject } : project
+      )
+    );
+    onTasks((prevTasks) => prevTasks.filter((task) => task.project !== id));
+    onAllTasks((prevTasks) => prevTasks.filter((task) => task.project !== id));
+
+    navigate("/app/today");
+  } catch (error) {
+    onSystemMessage("System encountered an error");
+    setTimeout(() => {
+      onSystemMessage(null);
+    }, 3000);
   }
 };
 export default handleDeleteProject;
