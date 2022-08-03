884e36f8ee152bbfa28a2cd01b14b9998065f1fb
dsabelli <dsabelli3@gmail.com>
2022-08-03 16:42:33 -0400

    sort and style timeMachine tasks

--- frontend/src/pages/Protected/TimeMachine.jsx
+++ frontend/src/pages/Protected/TimeMachine.jsx
@@ -1,16 +1,22 @@
 import { useEffect, useContext } from "react";
 import { useOutletContext, useParams } from "react-router-dom";
-import { UserContext } from "../../context/UserContext";
 import Task from "../../features/Tasks/Task";
 import { isBefore, parseJSON, isSameDay } from "date-fns";
 
 import { TimeMachineContext } from "../../context/TimeMachineContext";
 const TimeMachine = () => {
   let params = useParams();
-  const { user } = useContext(UserContext);
+
   const { timeMachineTasks } = useContext(TimeMachineContext);
   const [tasks, setTasks, allTasks, setAllTasks, projects] = useOutletContext();
-  console.log(timeMachineTasks);
+
+  const sortTasks = (a, b) => {
+    return a.completedOn < b.completedOn
+      ? 1
+      : a.completedOn > b.completedOn
+      ? -1
+      : 0;
+  };
   useEffect(() => {
     setTasks(
       allTasks.filter(
@@ -21,47 +27,69 @@ const TimeMachine = () => {
     );
   }, [params.date]);
 
-  const taskEls = tasks.map((task) => (
-    <Task
-      disabled="disabled"
-      hidden="hidden"
-      checked={task.isChecked}
-      title={task.title}
-      description={task.description}
-      dueDate={task.dueDate}
-      key={task.id}
-      id={task.id}
-      completedOn={task.completedOn}
-      important={task.isImportant}
-      project={task.project}
-      projects={projects}
-    />
-  ));
+  const taskEls = tasks
+    .sort(sortTasks)
+    .map((task) => (
+      <Task
+        disabled="disabled"
+        hidden="hidden"
+        checked={false}
+        title={task.title}
+        description={task.description}
+        dueDate={task.dueDate}
+        key={task.id}
+        id={task.id}
+        completedOn={null}
+        important={task.isImportant}
+        project={task.project}
+        projects={projects}
+      />
+    ));
 
   const timeMachineEls =
     timeMachineTasks.length > 0
-      ? timeMachineTasks.map((task) => (
-          <Task
-            disabled="disabled"
-            hidden="hidden"
-            checked={true}
-            title={task.title}
-            description={task.description}
-            dueDate={task.dueDate}
-            key={task.id}
-            id={task.id}
-            completedOn={task.completedOn}
-            important={task.isImportant}
-            project={task.project}
-            projects={projects}
-          />
-        ))
+      ? timeMachineTasks
+          .sort(sortTasks)
+          .map((task) =>
+            isSameDay(parseJSON(task.completedOn), parseJSON(params.date)) ? (
+              <Task
+                disabled="disabled"
+                hidden="hidden"
+                checked={true}
+                title={task.title}
+                description={task.description}
+                dueDate={task.dueDate}
+                key={task.id}
+                id={task.id}
+                completedOn={task.completedOn}
+                important={task.isImportant}
+                project={task.project}
+                projects={projects}
+              />
+            ) : (
+              <Task
+                disabled="disabled"
+                hidden="hidden"
+                checked={false}
+                title={task.title}
+                description={task.description}
+                dueDate={task.dueDate}
+                key={task.id}
+                id={task.id}
+                completedOn={null}
+                important={task.isImportant}
+                project={task.project}
+                projects={projects}
+              />
+            )
+          )
       : null;
 
   return (
     <div className="opacity-60">
-      {taskEls}
-      {timeMachineEls}
+      {taskEls.concat(timeMachineEls)}
+      {/* {taskEls}
+      {timeMachineEls} */}
     </div>
   );
 };
