923af57329f5d683d94753b9640abf84cbf174ad
dsabelli <dsabelli3@gmail.com>
2022-08-03 11:17:17 -0400

    add datepicker and navigation for timeMachineTasks

---
+++ frontend/src/features/Didits/TimeMachineSearch.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect, useContext } from "react";
+import DatePicker from "react-datepicker";
+import { startOfDay, endOfDay, subDays } from "date-fns";
+import "react-datepicker/dist/react-datepicker.css";
+import diditService from "../../services/didits";
+import { TimeMachineContext } from "../../context/TimeMachineContext";
+import { UserContext } from "../../context/UserContext";
+import { useNavigate } from "react-router-dom";
+const TimeMachineSearch = ({}) => {
+  let navigate = useNavigate();
+  const { user } = useContext(UserContext);
+  const { timeMachineTasks, setTimeMachineTasks } =
+    useContext(TimeMachineContext);
+  const [timeMachineDate, setTimeMachineDate] = useState(new Date());
+
+  //api call with state and debounced string query state.
+  //queries on change of date or debounced string.
+  useEffect(() => {
+    const getDidits = async () => {
+      const searchedDidits = await diditService.getDidits(
+        ".",
+        startOfDay(timeMachineDate),
+        endOfDay(timeMachineDate),
+        user
+      );
+
+      timeMachineDate && setTimeMachineTasks(searchedDidits);
+    };
+    getDidits();
+  }, [timeMachineDate]);
+
+  return (
+    <div className="w-44  ">
+      <DatePicker
+        maxDate={subDays(new Date(), 1)}
+        value="Select"
+        onChange={(date) => (
+          setTimeMachineDate(date),
+          navigate(`/app/time-machine/${date.toISOString()}`)
+        )}
+        className="w-full text-2xs md:text-xs btn pl-1"
+        popperPlacement="bottom-end"
+      />
+    </div>
+  );
+};
+
+export default TimeMachineSearch;
