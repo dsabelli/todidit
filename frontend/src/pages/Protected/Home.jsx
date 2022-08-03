cfde198f015b8d28524b26270837f57fd487bd8f
dsabelli <dsabelli3@gmail.com>
2022-08-03 11:16:10 -0400

    setup time machine context

--- frontend/src/pages/Protected/Home.jsx
+++ frontend/src/pages/Protected/Home.jsx
@@ -14,6 +14,7 @@ import { UserContext } from "../../context/UserContext";
 import { SettingsContext } from "../../context/SettingsContext";
 import { DateFormatContext } from "../../context/DateFormatContext";
 import { DiditContext } from "../../context/DiditContext";
+import { TimeMachineContext } from "../../context/TimeMachineContext";
 import Loader from "../../components/UI/Loader";
 import { getHeader } from "../../utils/headers";
 import SortButton from "../../layouts/SortButton";
@@ -37,6 +38,11 @@ const Home = ({ onTheme }) => {
     () => ({ didits, setDidits }),
     [didits, setDidits]
   );
+  const [timeMachineTasks, setTimeMachineTasks] = useState([]);
+  const timeMachineValue = useMemo(
+    () => ({ timeMachineTasks, setTimeMachineTasks }),
+    [timeMachineTasks, setTimeMachineTasks]
+  );
 
   let location = useLocation();
   let params = useParams();
@@ -47,7 +53,8 @@ const Home = ({ onTheme }) => {
   let showAddTask =
     location.pathname.includes("didit") ||
     location.pathname.includes("archive") ||
-    location.pathname.includes("completed");
+    location.pathname.includes("completed") ||
+    location.pathname.includes("time");
 
   //Checks if a user's token is stored in local storage
   //If it is, re-login is not required and token is parsed and set for use
@@ -133,90 +140,92 @@ const Home = ({ onTheme }) => {
 
   return loaded ? (
     <DiditContext.Provider value={diditValue}>
-      <Navbar projects={projects}>
-        <div>
-          <Menu tasks={allTasks} className="text-left text-xl py-6">
-            <ReadAndUpdateProjects
-              user={user}
-              tasks={tasks}
-              onTasks={setTasks}
-              allTasks={allTasks}
-              onAllTasks={setAllTasks}
-              projects={projects}
-              onProjects={setProjects}
-              projectTitle={projectTitle}
-              onProjectTitle={setProjectTitle}
-              onAddProject={setAddProject}
-              onSystemMessage={setSystemMessage}
-              navigate={navigate}
-            />
-            <CreateProject
-              user={user}
-              onProjects={setProjects}
-              projectTitle={projectTitle}
-              onProjectTitle={setProjectTitle}
-              addProject={addProject}
-              onAddProject={setAddProject}
-              onSystemMessage={setSystemMessage}
-            />
-            <ArchivedProjects projects={projects} />
-          </Menu>
-        </div>
-        <div>
-          {systemMessage && (
-            <div className="mt-6">
-              <ErrorMessage errorMessage={systemMessage} />
-            </div>
-          )}
-          <header className="max-w-3xl mx-auto flex justify-between items-center">
-            {getHeader(location, params, projects)}
-            {!showAddTask && (
-              <SortButton
-                sortBy={sortBy}
-                onSortBy={setSortBy}
-                order={order}
-                onOrder={setOrder}
+      <TimeMachineContext.Provider value={timeMachineValue}>
+        <Navbar projects={projects}>
+          <div>
+            <Menu tasks={allTasks} className="text-left text-xl py-6">
+              <ReadAndUpdateProjects
+                user={user}
                 tasks={tasks}
                 onTasks={setTasks}
                 allTasks={allTasks}
                 onAllTasks={setAllTasks}
+                projects={projects}
+                onProjects={setProjects}
+                projectTitle={projectTitle}
+                onProjectTitle={setProjectTitle}
+                onAddProject={setAddProject}
+                onSystemMessage={setSystemMessage}
+                navigate={navigate}
+              />
+              <CreateProject
+                user={user}
+                onProjects={setProjects}
+                projectTitle={projectTitle}
+                onProjectTitle={setProjectTitle}
+                addProject={addProject}
+                onAddProject={setAddProject}
+                onSystemMessage={setSystemMessage}
+              />
+              <ArchivedProjects projects={projects} />
+            </Menu>
+          </div>
+          <div>
+            {systemMessage && (
+              <div className="mt-6">
+                <ErrorMessage errorMessage={systemMessage} />
+              </div>
+            )}
+            <header className="max-w-3xl mx-auto flex justify-between items-center">
+              {getHeader(location, params, projects)}
+              {!showAddTask && (
+                <SortButton
+                  sortBy={sortBy}
+                  onSortBy={setSortBy}
+                  order={order}
+                  onOrder={setOrder}
+                  tasks={tasks}
+                  onTasks={setTasks}
+                  allTasks={allTasks}
+                  onAllTasks={setAllTasks}
+                />
+              )}
+            </header>
+            <Outlet
+              context={[
+                tasks,
+                setTasks,
+                allTasks,
+                setAllTasks,
+                projects,
+                setAddTask,
+                projectTitle,
+                setProjectTitle,
+                projectId,
+                setProjectId,
+                setSystemMessage,
+              ]}
+            />
+
+            {!showAddTask && (
+              <CreateTask
+                user={user}
+                addTask={addTask}
+                onAddTask={setAddTask}
+                onTasks={setTasks}
+                onAllTasks={setAllTasks}
+                onSystemMessage={setSystemMessage}
+                projects={projects}
+                projectTitle={projectTitle}
+                onProjectTitle={setProjectTitle}
+                projectId={projectId}
+                onProjectId={setProjectId}
+                tasks={tasks}
               />
             )}
-          </header>
-          <Outlet
-            context={[
-              tasks,
-              setTasks,
-              allTasks,
-              setAllTasks,
-              projects,
-              setAddTask,
-              projectTitle,
-              setProjectTitle,
-              projectId,
-              setProjectId,
-              setSystemMessage,
-            ]}
-          />
-
-          {!showAddTask && (
-            <CreateTask
-              user={user}
-              addTask={addTask}
-              onAddTask={setAddTask}
-              onTasks={setTasks}
-              onAllTasks={setAllTasks}
-              onSystemMessage={setSystemMessage}
-              projects={projects}
-              projectTitle={projectTitle}
-              onProjectTitle={setProjectTitle}
-              projectId={projectId}
-              onProjectId={setProjectId}
-              tasks={tasks}
-            />
-          )}
-        </div>
-      </Navbar>
+          </div>
+        </Navbar>
+      </TimeMachineContext.Provider>
     </DiditContext.Provider>
   ) : (
     <Loader />
