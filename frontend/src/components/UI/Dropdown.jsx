367e79b1016830bd9627a1f6d1505e1ab39adbaf
dsabelli <dsabelli3@gmail.com>
2022-08-03 20:05:04 -0400

    add text-class for dropdown label

--- frontend/src/components/UI/Dropdown.jsx
+++ frontend/src/components/UI/Dropdown.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-const Dropdown = ({ text, onClick, className, children }) => {
+const Dropdown = ({ text, onClick, className, children, textClass }) => {
   return (
     <div className={`dropdown ${className}`} onClick={onClick}>
       <label
         tabIndex="0"
-        className="text-xs flex flex-col items-start pb-2 cursor-pointer "
+        className={`text-xs flex flex-col items-start cursor-pointer ${
+          textClass ? textClass : "pb-2"
+        }`}
       >
         {text || children}
       </label>
