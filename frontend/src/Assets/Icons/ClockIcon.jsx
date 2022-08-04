6983d6a620d826fd5c36045aa2e28994384ac0ad
dsabelli <dsabelli3@gmail.com>
2022-08-04 08:54:27 -0400

    new icon asset

---
+++ frontend/src/Assets/Icons/ClockIcon.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ClockIcon = ({ className }) => {
+  return (
+    <div className={`text-base-content ${className}`}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="feather feather-clock"
+      >
+        <circle cx="12" cy="12" r="10"></circle>
+        <polyline points="12 6 12 12 16 14"></polyline>
+      </svg>
+    </div>
+  );
+};
+
+export default ClockIcon;
