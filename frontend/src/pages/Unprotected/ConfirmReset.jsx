ebc8b9b029ce15fd8f783ce0300dd4692b3a3339
dsabelli <dsabelli3@gmail.com>
2022-08-05 11:22:57 -0400

    remove page loader for btn loader

--- frontend/src/pages/Unprotected/ConfirmReset.jsx
+++ frontend/src/pages/Unprotected/ConfirmReset.jsx
@@ -10,8 +10,8 @@ import UNavbar from "../../layouts/UNavbar";
 import Hero from "../../components/UI/Hero";
 import ConfirmPWSvg from "../../Assets/SVGs/ConfirmPWSvg";
 import ConfirmResetSvg from "../../Assets/SVGs/ConfirmResetSvg";
-import Loader from "../../components/UI/Loader";
 import Footer from "../../components/UI/Footer";
+
 const schema = yup.object().shape({
   password: yup
     .string()
@@ -77,93 +77,89 @@ const ConfirmReset = ({}) => {
       }, 3000);
   }, [reset]);
 
-  return loaded ? (
-    reset ? (
-      <>
-        <UNavbar isLanding />
-        <Hero
-          className={"gap-20 items-stretch"}
-          text={
-            <>
-              <h1 className="text-4xl font-bold ">
-                Password reset
-                <br /> confirmed!
-              </h1>
-              <p className="mt-4 text-lg">Taking you to the login page...</p>
-            </>
-          }
-        >
-          <ConfirmResetSvg className="hidden md:block w-64" />
-        </Hero>
-      </>
-    ) : (
-      <>
-        <UNavbar />
-        <div className="md:hero min-h-screen">
-          <div className="hero-content flex-col md:flex-row-reverse gap-20">
-            <div className="text-center md:text-left">
-              <ConfirmPWSvg className={"hidden md:block w-80 mb-20"} />
-            </div>
-            <div className="mb-40">
-              <h1 className="text-5xl font-bold pb-4">
-                Confirm New
-                <br /> Password
-              </h1>
-              {asyncError && (
-                <p className="text-xs pb-2">
-                  {asyncError}{" "}
-                  <Link className="link" to="/reset-password">
-                    Request a new one?
-                  </Link>
-                </p>
-              )}
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                  <div className="card-body">
-                    <div className="form-control">
-                      <input
-                        type="password"
-                        name="password"
-                        {...register("password")}
-                        placeholder="new password"
-                        className="input input-bordered focus:outline-none"
+  return reset && loaded ? (
+    <>
+      <UNavbar isLanding />
+      <Hero
+        className={"gap-20 items-stretch"}
+        text={
+          <>
+            <h1 className="text-4xl font-bold ">
+              Password reset
+              <br /> confirmed!
+            </h1>
+            <p className="mt-4 text-lg">Taking you to the login page...</p>
+          </>
+        }
+      >
+        <ConfirmResetSvg className="hidden md:block w-64" />
+      </Hero>
+    </>
+  ) : (
+    <>
+      <UNavbar />
+      <div className="md:hero min-h-screen">
+        <div className="hero-content flex-col md:flex-row-reverse gap-20">
+          <div className="text-center md:text-left">
+            <ConfirmPWSvg className={"hidden md:block w-80 mb-20"} />
+          </div>
+          <div className="mb-40">
+            <h1 className="text-5xl font-bold pb-4">
+              Confirm New
+              <br /> Password
+            </h1>
+            {asyncError && (
+              <p className="text-xs pb-2">
+                {asyncError}{" "}
+                <Link className="link" to="/reset-password">
+                  Request a new one?
+                </Link>
+              </p>
+            )}
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card-body">
+                  <div className="form-control">
+                    <input
+                      type="password"
+                      name="password"
+                      {...register("password")}
+                      placeholder="new password"
+                      className="input input-bordered focus:outline-none"
+                    />
+                    {errors.password && (
+                      <ErrorMessage errorMessage={errors.password?.message} />
+                    )}
+                  </div>
+                  <div className="form-control">
+                    <input
+                      type="password"
+                      name="confirmPassword"
+                      {...register("confirmPassword")}
+                      placeholder="new confirm password"
+                      className="input input-bordered"
+                    />
+                    {errors.confirmPassword && (
+                      <ErrorMessage
+                        errorMessage={errors.confirmPassword?.message}
                       />
-                      {errors.password && (
-                        <ErrorMessage errorMessage={errors.password?.message} />
-                      )}
-                    </div>
-                    <div className="form-control">
-                      <input
-                        type="password"
-                        name="confirmPassword"
-                        {...register("confirmPassword")}
-                        placeholder="new confirm password"
-                        className="input input-bordered"
-                      />
-                      {errors.confirmPassword && (
-                        <ErrorMessage
-                          errorMessage={errors.confirmPassword?.message}
-                        />
-                      )}
-                    </div>
-                    <div className="form-control mt-6">
-                      <Button
-                        text={"Confirm"}
-                        type="submit"
-                        className={"btn-primary"}
-                      />
-                    </div>
+                    )}
+                  </div>
+                  <div className="form-control mt-6">
+                    <Button
+                      text={"Confirm"}
+                      type="submit"
+                      className={`btn-primary ${loaded ? "" : "loading"}`}
+                    />
                   </div>
                 </div>
-              </form>
-            </div>
+              </div>
+            </form>
           </div>
         </div>
-        <Footer />
-      </>
-    )
-  ) : (
-    <Loader />
+      </div>
+      <Footer />
+    </>
   );
 };
 
