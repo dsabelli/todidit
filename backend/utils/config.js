0a589a36b91a72d57d0061e3da2c77795486b9d8
dsabelli <dsabelli3@gmail.com>
2022-08-05 11:21:09 -0400

    change db to atlas

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT;
 const MONGODB_URI =
   process.env.NODE_ENV === "test"
     ? process.env.TEST_MONGODB_URI
-    : process.env.MONGODB_URI;
+    : process.env.MONGODB_CLOUD_URI;
 const SECRET = process.env.SECRET;
 const EMAIL_SECRET = process.env.EMAIL_SECRET;
 const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
