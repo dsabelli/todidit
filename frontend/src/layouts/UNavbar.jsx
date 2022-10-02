7c62a327a4c87afc580af7070b6adc836fbfce27
dsabelli <dsabelli3@gmail.com>
2022-10-02 17:01:48 -0400

    add button to login with test account session

--- frontend/src/layouts/UNavbar.jsx
+++ frontend/src/layouts/UNavbar.jsx
@@ -1,14 +1,63 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import UserDropdown from "./UserDropdown";
 import Button from "../components/UI/Button";
 import ToDidit from "../components/UI/ToDidit";
+import loginService from "../services/login";
+import taskService from "../services/tasks";
+import { UserContext } from "../context/UserContext";
+import emailjs from "@emailjs/browser";
 
 const UNavbar = ({ isLanding, username }) => {
+  let navigate = useNavigate();
+  const form = useRef();
+  const login_email = import.meta.env.VITE_LOGIN_EMAIL;
+  const login_password = import.meta.env.VITE_LOGIN_PASSWORD;
+  const emailJs_API = import.meta.env.VITE_EMAIL_JS_SITE_API_KEY;
+  const { setUser } = useContext(UserContext);
+
+  const handleLogin = async ({ email, password }) => {
+    try {
+      const user = await loginService.login({
+        email,
+        password,
+      });
+      window.sessionStorage.setItem("loggedIn", JSON.stringify(user));
+      taskService.setToken(user.token);
+      setUser(user);
+      navigate("/app/today");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const sendEmail = async (e) => {
+    e.preventDefault();
+    try {
+      await emailjs.sendForm(
+        "service_mmlaf1r",
+        "template_79xcu8k",
+        form.current,
+        emailJs_API
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <nav className="navbar bg-neutral flex px-4 justify-between h-16">
       <ToDidit className="pb-2" todiditClass="text-neutral-content" />
       <div className={`flex gap-4 ${isLanding ? "" : "hidden"} `}>
+        <form ref={form} onSubmit={(e) => sendEmail(e)}>
+          <Button
+            text={"try out test account"}
+            className="bg-secondary text-secondary-content hover:bg-secondary-focus "
+            onClick={() =>
+              handleLogin({ email: login_email, password: login_password })
+            }
+          />
+        </form>
         <Link to="/login">
           <Button
             text={"Login"}
